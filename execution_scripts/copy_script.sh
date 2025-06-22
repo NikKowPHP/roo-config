f505a729cb3e694b33d32f6eda37b62f5df2f87c
NikKowPHP <nik.kow@outlook.com>
2025-06-22 15:44:45 +0200

    add kilo code

--- execution_scripts/copy_script.sh
+++ execution_scripts/copy_script.sh
@@ -4,3 +4,5 @@
 cp .roo/custom_modes.yaml ~/.config/Code/User/globalStorage/rooveterinaryinc.roo-cline/settings/custom_modes.yaml
 cp .roo/custom_modes.yaml ~/Library/Application\ Support/Code/User/globalStorage/rooveterinaryinc.roo-cline/settings/
 cp .roo/custom_modes.yaml ~/AppData/Roaming/Code/User/globalStorage/rooveterinaryinc.roo-cline/settings/custom_modes.yaml
+
+cp .roo/custom_modes.yaml /Users/mikitakavaliou/Library/Application\ Support/Code/User/globalStorage/kilocode.kilo-code/settings/custom_modes.yaml
